1105b3baadfc222ef5d8fc9157cf1ba4d225ef51
niisharajput <135112243+niisharajput@users.noreply.github.com>
2023-10-04 00:55:11 +0530

    Add files via upload

---
+++ src/views/DashBoard.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../static/css/Dashboard.css";
+import back_img from "../static/images/logout.png";
+export default function Dashboard() {
+  const navigate = useNavigate();
+  const isLogged = sessionStorage.getItem("isLogged") || false;
+
+  // if (isLogged)
+    return (
+      <div className="Dashboard__main">
+        <nav className="navbar_ navbar_-expand-lg navbar_-light">
+          <a href="./log/login" className="logout">
+            <img src={back_img} className="logout-img" alt="" />
+          </a>
+          <div className="dashboard">Dashboard -</div>
+          <div className="dashboard__bg"></div>
+        </nav>
+
+        <div className="dashboard__cards">
+          <div
+            className="con"
+            id="ds__Todo"
+            onClick={() => {
+              navigate("/dashboard/todo");
+            }}
+          >
+            <div className="zayn_2 shadow">
+              <span className="ds__cover">White cover</span>
+              <h4>Todo</h4>
+            </div>
+          </div>
+          <div
+            className="con"
+            id="ds__FunZone"
+            onClick={() => {
+              navigate("/dashboard/fun");
+            }}
+          >
+            <div className="zayn_2 shadow">
+              <span className="ds__cover">White cover</span>
+              <h4>Fun</h4>
+            </div>
+          </div>
+          <div
+            className="con"
+            id="ds__Gallery"
+            onClick={() => {
+              navigate("/dashboard/gallery");
+            }}
+          >
+            <div className="zayn_2 shadow">
+              <span className="ds__cover">White cover</span>
+              <h4>Gallery</h4>
+            </div>
+          </div>
+          <div
+            className="con"
+            id="ds__Resources"
+            onClick={() => {
+              navigate("/dashboard/resource");
+            }}
+          >
+            <div className="zayn_2 shadow">
+              <span className="ds__cover">White cover</span>
+              <h4>Resources</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+  // else
+  //   return (
+  //     <div>Error 405</div>
+  //   )
+}
---
+++ src/views/SignUp.js
@@ -0,0 +1,146 @@
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
+
+import "../static/css/SignUp.css";
+import "../static/css/NavBar.css";
+import Navbar from "../utils/NavBar";
+import Footer from "../utils/Footer";
+
+function SignUp(props) {
+  const navigate = useNavigate();
+  const nameR = useRef(null);
+  const emailR = useRef(null);
+  const passwdR = useRef(null);
+  const cpasswdR = useRef(null);
+
+  const [Remember__me, setRemember__me] = useState("Remember me");
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    reEnterPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  
+  const rememberMeHandler = (e) => {
+    if(e.target.checked){
+      console.log("rememberinggg");
+      localStorage.setItem("isLogged", true);
+      localStorage.setItem("mainUser", user.name);
+      setRemember__me("Dont remember me");
+      return;
+    } else {
+      setRemember__me("Remember me");
+      localStorage.setItem("isLogged", false);
+      localStorage.setItem("mainUser", null);
+      return;
+    }
+  }
+
+  const register = (e) => {
+    e.preventDefault();
+    console.log(user);
+    if (
+      user.name &&
+      user.email &&
+      user.password &&
+      user.password === user.reEnterPassword
+    ) {
+      axios.post("/register", user).then((res) => {
+        alert(res.data.message);
+        if(res.status == 201){
+          navigate("/dashboard");
+          props.setMainUser(user.name);
+          sessionStorage.setItem("isLogged", true);
+          sessionStorage.setItem("mainUser", user.name);
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+      nameR.current.value = "";
+      emailR.current.value = "";
+      passwdR.current.value = "";
+      cpasswdR.current.value = "";
+    } else {
+      alert("Invalid inputs");
+      passwdR.current.value = "";
+      cpasswdR.current.value = "";
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div className="container">
+        <section id="content">
+          <form action="">
+            <h1>Signup Form</h1>
+            <div>
+              <input
+                type="text"
+                onChange={handleChange}
+                placeholder="Username"
+                required=""
+                ref={nameR}
+                id="username"
+                name="name"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                onChange={handleChange}
+                placeholder="Email"
+                ref={emailR}
+                id="email"
+                name="email"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                onChange={handleChange}
+                placeholder="Password"
+                required=""
+                ref={passwdR}
+                id="password"
+                name="password"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                onChange={handleChange}
+                placeholder="Repeat Password"
+                required=""
+                ref={cpasswdR}
+                id="cpassword"
+                name="reEnterPassword"
+              />
+            </div>
+            <a href="#" style={{ marginTop: "6px" }}>
+              <input type="button" value="Sign Up" onClick={() => {
+              navigate("/log/login");
+            }} />
+            </a>
+            <div>
+              <input type="checkbox" name="Remember_me" id="Remember_me" style={{ visibility : "none", display: "none"}} onChange={rememberMeHandler}/>
+              <label htmlFor="Remember_me">{Remember__me}</label>
+            </div>
+          </form>
+        </section>
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default SignUp;
